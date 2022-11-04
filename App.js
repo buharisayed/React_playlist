37b2d188af7ce426ba8cae57f2650356c77a3dba
Buhari Sayed <46406960+buharisayed@users.noreply.github.com>
2022-11-04 16:55:26 +0400

    Parallax

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import ImageOne from './components/ImageOne';
+import ImageTwo from './components/ImageTwo'
+import TextBox from './components/TextBox'
+import ImageThree from './components/ImageThree'
+function App() {
+  return (
+    <div className="App">
+    <ImageOne />
+    <TextBox />
+    <ImageTwo />
+    <TextBox />
+    <ImageThree />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ components/TextBox.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const TextBox = () => {
+  return (
+    <div>
+      <h3>First Class Ticket</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
+        soluta laborum eligendi pariatur modi sequi alias deleniti animi ipsa
+        natus nisi itaque omnis fugiat ex voluptate, quas magni placeat
+        inventore?Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+        velit ipsa non sit libero iste accusamus et mollitia repellendus quasi,
+        cumque eius Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Vero ipsam adipisci fugiat ea tempore expedita nesciunt corporis.
+        Cupiditate fuga, pariatur veniam, asperiores, officiis minima voluptate
+        natus atque necessitatibus fugiat alias. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Dolorem error est quo laborum eaque
+        deserunt beatae culpa natus rem, saepe suscipit, amet nam ducimus alias
+        officiis voluptatibus! Laudantium, et corporis? earum, fuga amet dolores
+        magnam optio incidunt illum!
+      </p>
+    </div>
+  );
+};
+
+export default TextBox;
